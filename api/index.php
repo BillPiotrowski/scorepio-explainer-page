52fdd738a201dda9ba79a6906f6048bd09ea380d
Bill Piotrowski <billpiotrowski@me.com>
2022-07-19 00:04:06 -0700

    Making API 1 to 1. One string returns one prediction.

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 $instances = htmlspecialchars($_GET["instances"]);
 
-$url = 'http://66.175.235.59:8501/v1/models/combat_detection_bert:predict';
+$url = 'http://localhost:8501/v1/models/combat_detection_bert:predict';
 
 $data = array("instances" => array($instances));
 
@@ -19,7 +19,19 @@ $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
 header("Content-Type: application/json");
-echo json_encode($result);
+
+if($result === FALSE) { 
+    echo json_encode(array("error" => "no response"));
+    exit();
+}
+
+$json = json_decode($result, true);
+$predictions = $json['predictions'];
+$prediction = $predictions[0][0];
+$response = array("prediction" => $prediction);
+
+echo json_encode($response);
+
 exit();
 
 ?>
